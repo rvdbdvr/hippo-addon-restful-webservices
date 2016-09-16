0d60cf87efadcb0a0f16f5947e529c92f22ef5ba
Reinier van den Born <reinier.van.den.born@hinttech.com>
2016-09-16 11:23:45 +0200

    Yet another change to support old JCR

--- src/main/java/org/onehippo/forge/webservices/jaxrs/jcr/util/JcrSessionUtil.java
+++ src/main/java/org/onehippo/forge/webservices/jaxrs/jcr/util/JcrSessionUtil.java
@@ -16,9 +16,18 @@
 
 package org.onehippo.forge.webservices.jaxrs.jcr.util;
 
+import javax.jcr.Item;
 import javax.jcr.LoginException;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Property;
+import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.hippoecm.repository.HippoRepository;
@@ -51,6 +60,7 @@ public class JcrSessionUtil {
      * @return a {@link javax.jcr.Session}
      * @throws LoginException
      */
+    /*
     public static Session createSession(String username, String password) throws LoginException {
         Session session = null;
         try {
@@ -63,5 +73,83 @@ public class JcrSessionUtil {
         }
         return session;
     }
+    */
 
+    public static Session createSession(String username, String password) throws LoginException {
+    	try {
+    		Repository repository = null;
+    		try {
+		    	Context initCtx       = new InitialContext();
+		    	Context envCtx        = (Context) initCtx.lookup("java:comp/env");
+		    	// retrieves the pooling repository by JNDI look up.
+		    	repository = (Repository) envCtx.lookup("hippo/repository");
+    		}
+    	    catch (NamingException e) {
+        		log.info( "Could not lookup repository from context, trying for local VM repo" );
+        		HippoRepository hipporepo = null;
+        		
+        		try {
+	    	    	hipporepo = HippoRepositoryFactory.getHippoRepository("vm://");
+        			repository = hipporepo.getRepository();
+        		}
+        		catch (RepositoryException re) {
+        			log.info( "Could not get local VM repo, trying default repo" );
+	    	    	hipporepo = HippoRepositoryFactory.getHippoRepository("rmi://localhost:1099/hipporepository");
+        			repository = hipporepo.getRepository();
+        		}
+        	}    		
+	    	Session session       = repository.login(new SimpleCredentials( username, password.toCharArray()));
+	    	return session;
+    	}
+
+    	catch (RepositoryException e) {
+    		log.error( "Could not log in repository", e);
+    		throw new LoginException( e );
+    	}
+    }
+    
+    public static Node getNode( Session session, String path ) throws RepositoryException {
+    	Item item = session.getItem( path );
+    	if (item instanceof Node) {
+    		return (Node) item;
+    	}
+    	throw new PathNotFoundException( "Node not found at "+path);
+    }
+    
+    
+    public static Property getProperty( Session session, String path ) throws RepositoryException {
+    	Item item = session.getItem( path );
+    	if (item instanceof Property) {
+    		return (Property) item;
+    	}
+    	throw new PathNotFoundException( "Property not found at "+path);
+    }
+        
+    public static boolean nodeExists( Session session, String path ) throws RepositoryException {
+    	if (session.itemExists( path )) {
+    		try {
+    			getNode( session, path );
+    		}
+    		catch (RepositoryException e) {
+    			return false;
+    		}
+    		return true;
+    		
+    	}
+    	return false;
+    }
+       
+    public static boolean propertyExists( Session session, String path ) throws RepositoryException {
+    	if (session.itemExists( path )) {
+    		try {
+    			getProperty( session, path );
+    		}
+    		catch (RepositoryException e) {
+    			return false;
+    		}
+    		return true;
+    		
+    	}
+    	return false;
+    }
 }
